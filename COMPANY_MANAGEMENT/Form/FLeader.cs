0c3b942abec9390ab3f04af967c96cf6e110a930
your_username <nguyenthanhloi260303@gmail.com>
2023-03-12 18:08:07 +0700

    update 12/3

---
+++ COMPANY_MANAGEMENT/Form/FLeader.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMPANY_MANAGEMENT
+{
+    public partial class FLeader : Form
+    {
+        public FLeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
