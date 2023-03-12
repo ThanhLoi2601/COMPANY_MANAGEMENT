abd8f2d804f04421cc721e1ee3722a7c5ecece1f
your_username <nguyenthanhloi260303@gmail.com>
2023-03-12 22:06:32 +0700

    update 12/3

--- COMPANY_MANAGEMENT/Form/FJob.cs
+++ COMPANY_MANAGEMENT/Form/FJob.cs
@@ -21,31 +21,39 @@ namespace COMPANY_MANAGEMENT
 
         private void btUpdate_Click(object sender, EventArgs e)
         {
-            if (this.check_Empty() == false)
+            if (this.check_Empty() == false && txtID.Text.Contains("JOB")== true)
             {
                 Job job = new Job(txtID.Text,txtName.Text,rTxtContent.Text,dtDateStart.Value,dtDateEnd.Value,int.Parse(txtBunus.Text));
                 jobDAO.Update(job);
-                dGVJobStaff.DataSource = jobDAO.LoadList();
+                dGVJobStaff.DataSource = jobDAO.LoadList("JOB");
+            }
+            else
+            {
+                MessageBox.Show("Please fill in information !! ");
             }
         }
 
         private void btInsert_Click(object sender, EventArgs e)
         {
-            if (this.check_Empty() == false)
+            if (this.check_Empty() == false && txtID.Text.Contains("JOB") == true)
             {
                 Job job = new Job(txtID.Text, txtName.Text, rTxtContent.Text, dtDateStart.Value, dtDateEnd.Value, int.Parse(txtBunus.Text));
                 jobDAO.Insert(job);
-                dGVJobStaff.DataSource = jobDAO.LoadList();
+                dGVJobStaff.DataSource = jobDAO.LoadList("JOB");
+            }
+            else
+            {
+                MessageBox.Show("Please fill in information !! ");
             }
         }
 
         private void btDelete_Click(object sender, EventArgs e)
         {
-            if (txtID.Text != "")
+            if (txtID.Text.Contains("JOB") == true)
             {
                 Job job = new Job(txtID.Text, txtName.Text, rTxtContent.Text, dtDateStart.Value, dtDateEnd.Value, int.Parse(txtBunus.Text));
                 jobDAO.Delete(job);
-                dGVJobStaff.DataSource = jobDAO.LoadList();
+                dGVJobStaff.DataSource = jobDAO.LoadList("JOB");
             }else
             {
                 MessageBox.Show("Please fill in ID !! ");
@@ -54,7 +62,8 @@ namespace COMPANY_MANAGEMENT
 
         private void FJob_Load(object sender, EventArgs e)
         {
-            dGVJobStaff.DataSource = jobDAO.LoadList();
+            dGVJobStaff.DataSource = jobDAO.LoadList("JOB");
+            dGVMyJob.DataSource = jobDAO.LoadList("JOM");
         }
 
         private bool check_Empty()
@@ -68,21 +77,39 @@ namespace COMPANY_MANAGEMENT
             return false;
         }
 
+        private void dGV_CellClick(DataGridView dGV)
+        {
+            int r = dGV.CurrentCell.RowIndex;
+            txtID.DataBindings.Clear();
+            txtID.Text = dGV.Rows[r].Cells[0].Value.ToString();
+            txtName.DataBindings.Clear();
+            txtName.Text = dGV.Rows[r].Cells[1].Value.ToString();
+            rTxtContent.DataBindings.Clear();
+            rTxtContent.Text = dGV.Rows[r].Cells[2].Value.ToString();
+            dtDateStart.DataBindings.Clear();
+            dtDateStart.Text = dGV.Rows[r].Cells[3].Value.ToString();
+            dtDateEnd.DataBindings.Clear();
+            dtDateEnd.Text = dGV.Rows[r].Cells[4].Value.ToString();
+            txtBunus.DataBindings.Clear();
+            txtBunus.Text = dGV.Rows[r].Cells[5].Value.ToString();
+        }
+
         private void dGVJobStaff_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            int r = this.dGVJobStaff.CurrentCell.RowIndex;
-            txtID.DataBindings.Clear();
-            txtID.Text = dGVJobStaff.Rows[r].Cells[0].Value.ToString();
-            txtName.DataBindings.Clear();
-            txtName.Text = dGVJobStaff.Rows[r].Cells[1].Value.ToString();
-            rTxtContent.DataBindings.Clear();
-            rTxtContent.Text = dGVJobStaff.Rows[r].Cells[2].Value.ToString();
-            dtDateStart.DataBindings.Clear();
-            dtDateStart.Text = dGVJobStaff.Rows[r].Cells[3].Value.ToString();
-            dtDateEnd.DataBindings.Clear();
-            dtDateEnd.Text = dGVJobStaff.Rows[r].Cells[4].Value.ToString();
-            txtBunus.DataBindings.Clear();
-            txtBunus.Text = dGVJobStaff.Rows[r].Cells[5].Value.ToString();
+            dGV_CellClick(this.dGVJobStaff);
+        }
+
+        private void dGVMyJob_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dGV_CellClick(this.dGVMyJob);
+        }
+
+        private void tabJobStaff_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabJobStaff.SelectedIndex == 0)
+                grInfo.Enabled = true;
+            else
+                grInfo.Enabled = false;
         }
     }
 }
